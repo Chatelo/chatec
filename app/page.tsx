26dffac39f5ff38a5b9f210569b0077c2ec3c966
Benard Ronoh <biikate48@gmail.com>
2024-08-27 21:56:32 +0300

    Separate Services list component

--- app/page.tsx
+++ app/page.tsx
@@ -2,40 +2,9 @@ import Link from "next/link";
 
 import StructuredData from "./components/StructuredData";
 import AnimatedServiceCard from "./components/AnimatedServiceCard";
+import { services } from "@/app/data/servicesList";
 
 export default function Home() {
-  const services = [
-    {
-      title: "Web Development",
-      description: `Code Your Success: Websites That Work Wonders
-We craft stunning, high-performance websites that captivate your audience and drive results. From sleek corporate sites to complex e-commerce platforms, our expert developers turn your digital dreams into reality. Experience the perfect blend of aesthetics and functionality that keeps visitors coming back for more.`,
-      icon: "/web.jpg",
-    },
-    {
-      title: "Mobile App Development",
-      description: `Apps That Make Waves: Your Idea, Our Innovation!
-Transform your concept into a cutting-edge mobile app that users can't put down. Our skilled developers create intuitive, feature-rich applications for iOS and Android that stand out in the crowded app marketplace. Whether you're streamlining business operations or launching the next big thing, we've got the expertise to make it happen.`,
-      icon: "/mobile.jpg",
-    },
-    {
-      title: "UI/UX Design",
-      description: `Design That Delights: Where Form Meets Function!
-We create user interfaces that don't just look good – they feel right. Our UI/UX wizards craft seamless, intuitive experiences that keep users engaged and coming back for more. From wireframes to polished products, we ensure every click, swipe, and tap is a pleasure for your audience.`,
-      icon: "/uiux.jpg",
-    },
-    {
-      title: "SEO",
-      description: `Boost Your Online Visibility: Dominate Search Results!We turn your website into a magnet for potential customers. Our cutting-edge SEO strategies propel your business to the top of search rankings, driving targeted traffic and supercharging your online presence. Don't just exist online - thrive and outshine your competition! Key features:
-Custom-tailored SEO plans
-Keyword domination
-Content optimization wizardry
-Technical SEO mastery
-Transparent reporting
-
-Ready to skyrocket your online success? Let's optimize your path to the top!`,
-      icon: "/seo.jpg",
-    },
-  ];
   const structuredData = {
     "@context": "https://schema.org",
     "@type": "Organization",
