ec0974b25b26f2009dad63846851741740de617f
Benard Ronoh <biikate48@gmail.com>
2024-08-27 14:38:36 +0300

    Fix searchbar length

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -15,17 +15,17 @@ export default function SearchBar({ onSearch }: SearchBarProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="mb-8 ">
+    <form onSubmit={handleSubmit} className="mb-8 flex flex-col sm:flex-row">
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Search posts..."
-        className="w-1/3 p-2 border rounded"
+        className="w-full sm:w-1/3 p-2 border rounded"
       />
       <button
         type="submit"
-        className="mt-2 bg-blue-500 text-white px-4 py-2 rounded"
+        className="mt-2 sm:mt-0 sm:ml-2 bg-blue-500 text-white px-4 py-2 rounded"
       >
         Search
       </button>
