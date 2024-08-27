33fa3d857d3a8fd3541a4cd0fb7bb9abee449f11
Benard Ronoh <biikate48@gmail.com>
2024-08-27 17:49:14 +0300

    Added graphics to services and rewrite description

--- app/components/AnimatedServiceCard.tsx
+++ app/components/AnimatedServiceCard.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 interface ServiceCardProps {
   title: string;
   description: string;
+  icon?: string;
 }
 
 export default function AnimatedServiceCard({
   title,
   description,
+  icon,
 }: ServiceCardProps) {
   return (
     <motion.div
@@ -18,6 +21,11 @@ export default function AnimatedServiceCard({
       transition={{ duration: 0.5 }}
       className="bg-skin-fill text-skin-base p-6 rounded-lg shadow-md"
     >
+      {icon && (
+        <div className="mb-4">
+          <Image src={icon} alt={title} width={600} height={100} />
+        </div>
+      )}
       <h2 className="text-2xl font-semibold mb-4">{title}</h2>
       <p>{description}</p>
     </motion.div>
