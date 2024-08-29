6fab16c09ffaf72ed4ceaafda6282bc41544d5c8
Benard Ronoh <biikate48@gmail.com>
2024-08-30 00:47:48 +0300

    Added Opengraph image

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,11 +14,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
     },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/signin`,
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/register`,
-    },
   ];
 }
