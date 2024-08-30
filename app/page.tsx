5b149ab6ed2d43273d708e7fe2c2b31b969920e9
Benard Ronoh <biikate48@gmail.com>
2024-08-30 22:31:11 +0300

    Service Detail Page - for Web applications

--- app/page.tsx
+++ app/page.tsx
@@ -23,21 +23,25 @@ export default function Home() {
       <StructuredData data={structuredData} />
 
       <div className="container mx-auto px-6 py-16">
-        <h1 className="text-4xl md:text-6xl font-bold mb-8 animate-fade-in pt-12">
-          Building the Future of Web and Mobile
-        </h1>
-        <p className="text-xl mb-12 animate-fade-in-delay">
-          We create cutting-edge applications that drive business growth and
-          user engagement as well personal blogs.
-        </p>
-        <Link
-          href="/pricing"
-          className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300 animate-fade-in-delay-2"
-        >
-          Get Started
-        </Link>
-        <div className="pt-36">
-          <h2 className="text-4xl font-bold mb-12">Services we offer</h2>
+        <div className="bg-teal-500 px-12 pb-8">
+          <h1 className="text-4xl md:text-6xl font-bold mb-8 animate-fade-in pt-12">
+            Building the Future of Web and Mobile
+          </h1>
+          <p className="text-xl mb-12 animate-fade-in-delay">
+            We create cutting-edge applications that drive business growth and
+            user engagement as well personal blogs.
+          </p>
+          <Link
+            href="/pricing"
+            className="bg-blue-500 text-white px-6 py-3 te rounded-lg hover:bg-blue-600 transition duration-300 animate-fade-in-delay-2"
+          >
+            Get Started
+          </Link>
+        </div>
+        <div className="pt-6">
+          <h2 className="text-4xl font-bold text-center mb-6">
+            Services we offer
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             {services.map((service, index) => (
               <AnimatedServiceCard
