94f0c0113a99146447c9e6e08681c1837c86e489
Benard Ronoh <biikate48@gmail.com>
2024-09-01 17:14:40 +0300

    Removed services

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,9 +3,6 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/services`,
-    },
-    {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
     },
     {
