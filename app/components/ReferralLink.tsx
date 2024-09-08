d014fe4a915bfdac1f07aa872a96f5b0b80ecdf4
Benard Ronoh <biikate48@gmail.com>
2024-09-08 23:41:02 +0300

    Fixed bg dashboard

--- app/components/ReferralLink.tsx
+++ app/components/ReferralLink.tsx
@@ -16,7 +16,7 @@ export const ReferralLink: React.FC<ReferralLinkProps> = ({ referralLink }) => {
   };
 
   return (
-    <div className="bg-white shadow rounded-lg p-4 mt-4">
+    <div className="bg-skin-fill text-skin-base shadow rounded-lg p-4 mt-4">
       <h2 className="text-lg font-semibold mb-2">Your Referral Link</h2>
       <div className="flex items-center">
         <input
