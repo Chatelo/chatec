edced9929da4b25e55eb6451595e20f8497e97e8
Benard Ronoh <biikate48@gmail.com>
2024-09-18 00:04:15 +0300

    Terms and conditions

--- app/lib/email.ts
+++ app/lib/email.ts
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   // service: "gmail",
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT || "587"),
-  secure: process.env.SMTP_PORT === "465",
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
