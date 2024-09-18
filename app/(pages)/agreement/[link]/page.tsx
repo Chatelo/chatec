897b8908a5170f188fa4ad4b7bf686f40e4a3636
Benard Ronoh <biikate48@gmail.com>
2024-09-18 21:17:08 +0300

    Made changes to generic  company name to real company name

--- app/(pages)/agreement/[link]/page.tsx
+++ app/(pages)/agreement/[link]/page.tsx
@@ -72,7 +72,7 @@ function ErrorMessage({ message }: { message: string }) {
         className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
         role="alert"
       >
-        <strong className="font-bold">Error: </strong>
+        {/* <strong className="font-bold">Error: </strong> */}
         <span className="block sm:inline">{message}</span>
       </div>
     </div>
