8879f46df8b02b3c175c94848792e19161a071d8
Benard Ronoh <biikate48@gmail.com>
2024-09-29 16:34:00 +0300

    Escaping characters

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,8 +1,9 @@
 import PrismaClient from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
+// import { withAccelerate } from "@prisma/extension-accelerate";
 
-export const prisma = new PrismaClient.PrismaClient().$extends(
-  withAccelerate()
-);
+// export const prisma = new PrismaClient.PrismaClient().$extends(
+//   withAccelerate()
+// );
 
+export const prisma = new PrismaClient.PrismaClient();
 export default prisma;
