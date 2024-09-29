2c45c32d6bd6274fd4e4a30ab65de3453d5d64f8
Benard Ronoh <biikate48@gmail.com>
2024-09-29 16:35:21 +0300

    Escaping characters

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -5,5 +5,7 @@ import PrismaClient from "@prisma/client";
 //   withAccelerate()
 // );
 
+// TODO to enable accelerate, uncomment the following lines and fix "prisma" type error in the app
+
 export const prisma = new PrismaClient.PrismaClient();
 export default prisma;
